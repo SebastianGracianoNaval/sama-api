499e84c05e76556895f3c938dc012d6ddc149ebd
Sebastian Graciano <sebastiangracianonaval@gmail.com>
2025-06-11 21:44:52 -0300

    Eliminar carpeta api

--- api/consolidar.js
+++
@@ -1,9 +0,0 @@
-const { consolidarArchivos } = require('../controllers/webhookController');
-
-module.exports = (req, res) => {
-  if (req.method === 'POST') {
-    consolidarArchivos(req, res);
-  } else {
-    res.status(405).json({ success: false, message: 'Método no permitido' });
-  }
-}; 
