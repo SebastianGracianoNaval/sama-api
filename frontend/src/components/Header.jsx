1017fc04a905632064d4780fac86c1457e970ae1
Sebastian Graciano <sebastiangracianonaval@gmail.com>
2025-06-15 14:58:02 -0300

    Refactor exitoso - Cambios en el Front: BeHookedv2

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => (
   <AppBar position="static" sx={{ bgcolor: '#111', boxShadow: 2 }}>
     <Toolbar>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-        <span style={{ fontSize: 32 }}>🎣</span>
+        <img src="/hook_icon_svg.svg" alt="BeHooked Logo" style={{ height: 36 }} />
         <Typography variant="h5" component="div" sx={{ fontWeight: 700, letterSpacing: 2 }}>
           BeHooked
         </Typography>
